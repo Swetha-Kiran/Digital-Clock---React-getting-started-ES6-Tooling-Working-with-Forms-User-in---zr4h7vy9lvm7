f878465d258090ea6c3f4037d5be359e0b4545c9
Swetha Reddy <swethakiranmv@gmail.com>
2023-04-01 12:24:16 +0530

    clock

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,28 @@
 import React from 'react'
 import '../styles/App.css';
-const App = () => {
+import { useState, useEffect } from 'react';
 
+const App = () => {
+  const [date, setDate] = useState(new Date());
+  function refreshClock() {
+    setDate(new Date());
+  }
+  useEffect(() => {
+    const timerId = setInterval(refreshClock, 1000);
+    return function cleanup() {
+      clearInterval(timerId);
+    };
+  }, []);
   return (
     <div id="main">
-      <div className="date-time"></div>
+      <div className="date-time">
+      {date.toLocaleTimeString()}
+      </div>
     </div>
   )
 }
 
 
 export default App;
+  
+
